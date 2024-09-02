2d96968f1cb091a845278007502b2ab0fb34503f
Mudassar Bagwan <mudassarbagwan26@gmail.com>
2024-09-02 20:13:52 +0530

    first commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,15 +3,6 @@ import heroImg from './assets/hero.svg'
 const Hero = () => {
   return (
     <section className='hero'>
-      <div className="hero-center">
-        <div className="hero-title">
-            <h1>Projects</h1>
-            
-        </div>
-        <div className="img-container">
-            <img  src={heroImg} alt="heroImg"/>
-        </div>
-      </div>
     </section>
   )
 }
